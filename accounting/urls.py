a152c12d2aea72ca85ed563b50f5a740c9bfc33b
Sarik Kumpan <a5810164@gmail.com>
2017-02-16 18:18:49 +0700

    add comment and fix bug not match for args ['1','3'] when revers

--- accounting/urls.py
+++ accounting/urls.py
@@ -7,12 +7,14 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^add_user_page/$', views.add_user_page, name='add_user_page'),
     url(r'^add_user_func/$', views.add_user_func, name='add_user_func'),
-    url(r'^(?P<pk>[0-9]+)/page_add_book/$', views.BookManage.as_view(), name='book_manage'),
+    # generic
+    url(r'^(?P<pk>[0-9]+)/page_add_book/$', views.BookManage.as_view(), name='book_manage'), # add or delete book
     url(r'^(?P<pk>[0-9]+)/user_book/$', views.UserBook.as_view(), name='user_book'),
-    url(r'^(?P<pk>[0-9]+)/book_detail/$', views.BookDetail.as_view(), name='book_detail'),
+    url(r'^(?P<pk>[0-9][0-9]+)/book_detail/$', views.BookDetail.as_view(), name='book_detail'), # show all list in a book
     url(r'^(?P<pk>[0-9]+)/AddList/$', views.AddList.as_view(), name='AddList'),
-    url(r'^(?P<pk>[0-9]+)/TypeManage/$', views.TypeManage.as_view(), name="type_manage"),
+    url(r'^(?P<pk>[0-9]+)/TypeManage/$', views.TypeManage.as_view(), name="type_manage"), # add or delete tpye
     url(r'^(?P<pk>[0-9]+)/page_add_type/$', views.PageAddType.as_view(), name='page_add_type'),
+    # have args , and is'n generic
     url(r'^(?P<user_id>[0-9]+)/func_add_type/$', views.func_add_type, name='func_add_type'),
     url(r'^(?P<user_id>[0-9]+)/func_add_book/$', views.func_add_book, name='func_add_book'),
     url(r'^(?P<book_id>[0-9]+)/func_add_list/$', views.func_add_list, name='func_add_list'),
--- accounting/views.py
+++ accounting/views.py
@@ -21,22 +21,22 @@ class BookManage(generic.DetailView): # for add or delete book
     template_name = 'accounting/book_manage.html'
 
 
-class BookDetail(generic.DetailView):
+class BookDetail(generic.DetailView): # show all list in a book
     model = Pass_book
     template_name = 'accounting/book_detail.html'
 
 
-class AddList(generic.DetailView):
+class AddList(generic.DetailView): # go to add list page
     model = Pass_book
     template_name = 'accounting/add_list_page.html'
 
 
-class TypeManage(generic.DetailView):
+class TypeManage(generic.DetailView): # go to add or delete type page
     model = User
     template_name = 'accounting/type_manage.html'
 
 
-class PageAddType(generic.DetailView):
+class PageAddType(generic.DetailView): # go to add type page
     model = User
     template_name = 'accounting/add_type.html'
 
@@ -48,55 +48,56 @@ def add_user_func(request): # function add user and return to main page
     text_name = request.POST.get('user_name')
     new_user = User(user_name = text_name, founded=timezone.now())
     new_user.save()
-    return HttpResponseRedirect(reverse('accounting:user_book', args=str(new_user.id)))
+    return HttpResponseRedirect(reverse('accounting:user_book', args=[str(new_user.id)]))
 
-def func_add_book(request, user_id):
-    pass_book_name = request.POST.get('book_name')
-    user = User.objects.get(pk=user_id)
-    user.pass_book_set.create(book_name=pass_book_name, founded=timezone.now())
-    return HttpResponseRedirect(reverse('accounting:user_book', args=(user_id)))
+def func_add_book(request, user_id): # add a book to a user
+    pass_book_name = request.POST.get('book_name') # get name of pass book from input
+    user = User.objects.get(pk=user_id) # get user by using user_id 
+    user.pass_book_set.create(book_name=pass_book_name, founded=timezone.now()) # make new pass book for user
+    return HttpResponseRedirect(reverse('accounting:user_book', args=[user_id])) 
 
 def func_add_list(request, book_id):
+    # get input from user sended
     list_name_in = request.POST.get('list_name')
     list_detail_in = request.POST.get('detail')
     list_value_in = request.POST.get('value')
     list_type_in = request.POST.get('type_text')
     list_type_for_in = request.POST.get('type_for')
-    list_date_in = request.POST.get('date')
+    list_date_in = request.POST.get('date') 
     # start formate mm/dd/yyyy to yyyy-mm-dd
     year_in = list_date_in[6:]
     day_in = list_date_in[3:5]
     month_in = list_date_in[0:2]
     list_date_in = year_in + "-" + month_in + "-" + day_in
     # end formate to yyyy-mm-dd
-    book = Pass_book.objects.get(pk=book_id)
-    try:
-        list_instant = List_type.objects.get(type_name=list_type_in)
-    except:
-         user = User.objects.get(pk=book.user.id)
+    book = Pass_book.objects.get(pk=book_id) # get book by using book_id 
+    try:         # try if dont have type it will make new one for use
+        list_instant = List_type.objects.get(type_name=list_type_in) # have any list_type_name like input?
+    except:      # no have any list_type_name like input , or other reason
+         user = User.objects.get(pk=book.user.id) # get user object by use use_id
          user.list_type_set.create(type_name=list_type_in,\
-                                       type_for=list_type_for_in)
-         list_instant = List_type.objects.get(type_name=list_type_in)
+                                    type_for=list_type_for_in)     # make new list_type
+         list_instant = List_type.objects.get(type_name=list_type_in) 
     book.list_set.create(list_name=list_name_in,\
                              detail=list_detail_in,\
                              value=list_value_in,\
                              list_type=list_instant,\
                              date = list_date_in,\
-                             )
-    if(list_type_for_in == 'income'):
+                             )         # make new list for a book
+    if(list_type_for_in == 'income'):  # check type of list if in come + balance other -
         book.balance += float(list_value_in)
     else:
         book.balance -= float(list_value_in)
     book.save()
-    return HttpResponseRedirect(reverse('accounting:book_detail', args=(book_id),))
+    return HttpResponseRedirect(reverse('accounting:book_detail', args=[book.id]))
 
 
 def func_add_type(request, user_id):
-    user = User.objects.get(pk=user_id)
-    type_name_in = request.POST.get('type_name')
-    type_detail_in = request.POST.get('detail')
-    type_for_in = request.POST.get('type_for')
+    user = User.objects.get(pk=user_id) # find user object by user_id
+    type_name_in = request.POST.get('type_name')  # get input typename
+    type_detail_in = request.POST.get('detail') # get input detail
+    type_for_in = request.POST.get('type_for') # get input type_for
     user.list_type_set.create(type_name=type_name_in,\
                                   type_detail=type_detail_in,\
-                                  type_for=type_for_in)
-    return HttpResponseRedirect(reverse('accounting:type_manage', args=(user_id)))
+                                  type_for=type_for_in) # make new list type
+    return HttpResponseRedirect(reverse('accounting:type_manage', args=[user_id]))
