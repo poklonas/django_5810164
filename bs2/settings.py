4842c34368d98377873365f0a87b471e80fa92a8
Sarik Kumpan <a5810164@gmail.com>
2017-02-22 03:54:08 +0700

    add new app name homepage for use show homepage link to another app and my blog

--- bs2/settings.py
+++ bs2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
 	'accounting.apps.AccountingConfig',
     'bmi.apps.BmiConfig',
     'django.contrib.admin',
--- bs2/urls.py
+++ bs2/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('homepage.urls')),
 	url(r'^accounting/', include('accounting.urls')),
     url(r'^bmi/', include('bmi.urls')),
     url(r'^admin/', admin.site.urls),
