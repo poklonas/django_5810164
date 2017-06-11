059e99505e5433ff42cffa7c8ac8b9c35d769c09
Sarik Kumpan <a5810164@gmail.com>
2017-06-11 19:30:35 +0700

    update app schedule

--- schedule/views.py
+++ schedule/views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render, redirect
 from schedule.models import User, Activity
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User as User_id
+from django.contrib.auth import authenticate, login, logout
 
 
 ''' if this page has value from form it will creat a user object
 	and redirect to it self '''
 def home_page(request):
-    user_list = User.objects.all()
-    if request.method == 'POST':
-        user = User.objects.create(name=request.POST['user_name'])
+    if request.user.is_authenticated:
+        user_pk = (User.objects.get(mail=request.user.email)).pk
+        return redirect(reverse('schedule:user_page', kwargs={'user_id':user_pk}))
+    else:
+        if request.method == 'POST':
+            email = request.POST['login_email']
+            password = request.POST['login_password']
+            user_name = User_id.objects.get(email=email.lower()).username
+            user = authenticate(username=user_name, password=password)
+            if user is not None:
+                user_pk = (User.objects.get(mail=email)).pk
+                login(request, user)
+                return redirect(reverse('schedule:user_page', kwargs={'user_id':user_pk}))
+            else:
+                return render(request, 'schedule/homepage.html', {'error_messege_login':"email or password was worng"})
+        return render(request, 'schedule/homepage.html')
+
+def logout_views(request):
+    logout(request)
+    return redirect(reverse('schedule:home_page'))
+
+def create_user(request):
+    new_email = request.POST['email']
+    user_name = request.POST['user_name']
+    user_pass = request.POST['password']
+    try: # if error that mean dont have any user object which have mail = new mail
+        check_user_email = User.objects.get(mail=new_email)
+    except: # so make it new one
+        user = User.objects.create(name=user_name, mail=new_email)
         user.save()
         generate_activity_for_first_time_of_user('Monday', user.pk)
         generate_activity_for_first_time_of_user('Tuesday', user.pk)
@@ -17,8 +46,12 @@ def home_page(request):
         generate_activity_for_first_time_of_user('Friday', user.pk)
         generate_activity_for_first_time_of_user('Saturday', user.pk)
         generate_activity_for_first_time_of_user('Sunday', user.pk)
+        new_id = User_id.objects.create_user(user_name,
+                                             new_email,
+                                             user_pass)
+        new_id.save()
         return redirect(reverse('schedule:home_page'))
-    return render(request, 'schedule/homepage.html', {'user_list': user_list})
+    return render(request, 'schedule/homepage.html', {'error_messege_new_user':"That email was alredy used"})
 
 def generate_activity_for_first_time_of_user(day, user_id):
     user = User.objects.get(pk=user_id)
@@ -27,36 +60,47 @@ def generate_activity_for_first_time_of_user(day, user_id):
                                 time=count,
                                 day=day)
 
+def check_email_login(request, user_page_mail):
+    return (request.user.email == user_page_mail)
+
+@login_required(login_url='/schedule')
 def user_page(request, user_id):
     user = User.objects.get(pk=user_id)
-    return render(request, 'schedule/userpage.html', {'user': user})
+    if(check_email_login(request, user.mail)):
+        return render(request, 'schedule/userpage.html', {'user': user})
+    else:
+        return render(request, 'schedule/logoutpage.html')
 
+@login_required(login_url='/schedule')
 def add_new_activity(request, user_id):
     user = User.objects.get(pk=user_id)
-    start_time = int(request.POST['start_time'])
-    how_many_hour = int(request.POST['how_many_hour'])
-    max_time = start_time + how_many_hour
-    day_in = request.POST['day_selecter']
-    detail_in = request.POST['detail']
-    for count_time in range(start_time, max_time):
-        activity_filter = Activity.objects.get(user=user,
-                                               time=count_time,
-                                               day=day_in,)
-        if(activity_filter.connected): # if True that mean it should remove old activity first
-            reset_same_time_activity_reverse(user_id, count_time, day_in)
+    if(check_email_login(request, user.mail)):
+        start_time = int(request.POST['start_time'])
+        how_many_hour = int(request.POST['how_many_hour'])
+        max_time = start_time + how_many_hour
+        day_in = request.POST['day_selecter']
+        detail_in = request.POST['detail']
+        for count_time in range(start_time, max_time):
+            activity_filter = Activity.objects.get(user=user,
+                                                   time=count_time,
+                                                   day=day_in,)
+            if(activity_filter.connected): # if True that mean it should remove old activity first
+                reset_same_time_activity_reverse(user_id, count_time, day_in)
 
-        if(activity_filter.detail != ""): # if That True that mean it find head of activity
-            reset_same_time_activity_forward(user_id, count_time, day_in, activity_filter.time_left)
+            if(activity_filter.detail != ""): # if That True that mean it find head of activity
+                reset_same_time_activity_forward(user_id, count_time, day_in, activity_filter.time_left)
 
-        activity_filter.setDetail(detail_in)
-        activity_filter.set_time_left(how_many_hour)
-        if(count_time == start_time): # if that is first set it head for make colum span
-            activity_filter.set_connected(False)
-        else:
-            activity_filter.set_connected(True)
-        how_many_hour = how_many_hour - 1
-        activity_filter.save()
-    return redirect(reverse('schedule:user_page', args=[user_id],))
+            activity_filter.setDetail(detail_in)
+            activity_filter.set_time_left(how_many_hour)
+            if(count_time == start_time): # if that is first set it head for make colum span
+                activity_filter.set_connected(False)
+            else:
+                activity_filter.set_connected(True)
+            how_many_hour = how_many_hour - 1
+            activity_filter.save()
+        return redirect(reverse('schedule:user_page', args=[user_id],))
+    else:
+        return render(request, 'schedule/logoutpage.html')
 
 def reset_same_time_activity_reverse(user_id, time, day): #count-down to reset
     user = User.objects.get(pk=user_id)
@@ -104,23 +148,27 @@ def find_head_from_connected(user_id, time, day):
     else:
         return time
 
+@login_required(login_url='/schedule')
 def confirm_delete(request, user_id):
     user = User.objects.get(pk=user_id)
-    start_time = int(request.POST['start_time'])
-    how_many_hour = int(request.POST['how_many_hour'])
-    day_in = request.POST['day_selecter']
-    detail_in = request.POST['detail']
-    max_time = start_time + how_many_hour
-    if(max_time > 24):
-        return render(request, 'schedule/userpage.html',
-                               {'user': user,
-                                'error_messege':"Your select time after 23.00 It cant , please Try another time"})
-    collide = check_has_same_time(user_id, start_time, day_in, how_many_hour)
-    if(collide == []):
-        return add_new_activity(request, user_id)
-    return render(request, 'schedule/confirm_add.html',{'user': user,
-                                                        'collide_time':collide,
-                                                        'day':day_in,
-                                                        'start':start_time,
-                                                        'how_many_hour':how_many_hour,
-                                                        'detail_in':detail_in})
+    if(check_email_login(request, user.mail)):
+        start_time = int(request.POST['start_time'])
+        how_many_hour = int(request.POST['how_many_hour'])
+        day_in = request.POST['day_selecter']
+        detail_in = request.POST['detail']
+        max_time = start_time + how_many_hour
+        if(max_time > 24):
+            return render(request, 'schedule/userpage.html',
+                                   {'user': user,
+                                    'error_messege':"Your select time after 23.00 It cant , please Try another time"})
+        collide = check_has_same_time(user_id, start_time, day_in, how_many_hour)
+        if(collide == []):
+            return add_new_activity(request, user_id)
+        return render(request, 'schedule/confirm_add.html',{'user': user,
+                                                            'collide_time':collide,
+                                                            'day':day_in,
+                                                            'start':start_time,
+                                                            'how_many_hour':how_many_hour,
+                                                            'detail_in':detail_in})
+    else:
+        return render(request, 'schedule/logoutpage.html')
